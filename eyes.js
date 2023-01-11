b1b67e85e31cba5416479ce61929694f89c4bdb1
tareim <119832950+tareim@users.noreply.github.com>
2023-01-10 23:26:37 -0800

    Update eyes.js

--- eyes.js
+++ eyes.js
@@ -1,4 +1,4 @@
-const balls = document.getElementsByClassName('ball');
+//const balls = document.getElementsByClassName('ball');
 document.onmousemove = (event) => {
   const x = (event.clientX * 100) / window.innerWidth + '%';
   const y = (event.clientY * 100) / window.innerHeight + '%';
